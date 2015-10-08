3e0bf1f3ed98dec7121a8f7748924db063a23878
Jörg Weber <weber.joerg.h@gmail.com>
2015-10-08 10:38:45 +0200

    * add sample code to handle different deploy 	modes and serve the correct main html

--- src/main/java/org/dicadeveloper/weplantaforest/StaticDevelopmentContent.java
+++ src/main/java/org/dicadeveloper/weplantaforest/StaticDevelopmentContent.java
@@ -7,9 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class StaticDevelopmentContent extends WebMvcConfigurerAdapter {
 
+    //private Log log = LogFactory.getLog(StaticDevelopmentContent.class);
+    
+    //@Value("${mode}")
+    //private String _mode;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        /**
+         *  TODO Not needed right now but becomes handy if we can manage auto reload triggered by UI
+         *  In this case the "index.dev.html" will have additional script for reloading the content
+         */
+        // String viewName = _mode == "dev" ? "redirect:/index.dev.html" : "redirect:/index.html";
+        // log.info("Main webapp entry point is: " + viewName);
         registry.addViewController("/").setViewName("redirect:/index.html");
     }
 }
